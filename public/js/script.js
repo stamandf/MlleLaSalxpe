bb2e6aa9aef5ac56f2c0f5f2727d2ad31d3b403a
Florence <stamandf@gmail.com>
2019-11-01 12:57:44 -0400

    Add height to small logo.  Fixed bug with sticky menu. Add scroll animation.

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,83 @@ $(document).ready(function () {
     }, {
         offset: '60px'
     });
+    
+    //Scroll when click on links
+    $('js--scroll-to-product').click(function() {
+      $('html,body').animate({scrollTop: $('js--section-product').offset(),top}, 3000);
+    });
 
+    $('js--scroll-to-vision').click(function() {
+      $('html,body').animate({scrollTop: $('js--section-vision').offset(),top}, 3000);
+    });
 
+    /* Navigation scroll */
+    $(function() {
+      // Select all links with hashes
+      $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+          // On-page links
+          if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+          ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+              // Only prevent default if animation is actually gonna happen
+              event.preventDefault();
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000, function() {
+                // Callback after animation
+                // Must change focus!
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // Checking if the target was focused
+                  return false;
+                } else {
+                  $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                  $target.focus(); // Set focus again
+                };
+              });
+            }
+          }
+        });
+      });
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated fadeInRight');
+    }, {
+        offset: '50%'
+    });
 });
