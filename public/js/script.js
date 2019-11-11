7ed6b8e5b93a55cbbc9a3fa5016fd933595e7a94
Florence <stamandf@gmail.com>
2019-11-11 15:52:19 -0500

    Test with debugging and traces.

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
     /* For the sticky navigation */
+    let distance = $('.js--section-product').offset().top; //test
+    let $window = $(window);
+    // let productSection = $('.js--section-product');
+    console.log('distance1=', distance); //TRACE
+    // console.log('window scrollTop=', $window.scrollTop());
+    // console.log('productSection=', productSection.scrollTop());
+    // if ($window.scrollTop() >= distance) {
+    //   console.log("product section reached top!")
+    // }
     $('.js--section-product').waypoint(function(direction) {
         if (direction == "down") {
           $('header').addClass('sticky');
+          // console.log('distance2=', distance); //TRACE
+          // console.log('productSection2=', productSection.scrollTop());
+          console.log('window scrollTop2=', $window.scrollTop());
+          
+          // if ($window.scrollTop() >= distance) {
+          //   console.log("product section reached top!")
+          // }
           // $('.header__logo').addClass('sticky');
           // $('.header__logo').append('sticky');
         } else {
@@ -11,7 +27,7 @@ $(document).ready(function () {
           // $('.header__logo').remove('sticky');
         }
     }, {
-        offset: '30%'
+        offset: '20%'
     });
     
     // //Scroll when click on links
@@ -45,23 +61,24 @@ $(document).ready(function () {
           ) {
             // Figure out element to scroll to
             var target = $(this.hash);
-            console.log('location.hostname=', location.hostname); //TRACE
-            console.log('target1=', target); //TRACE
+            // console.log('location.hostname=', location.hostname); //TRACE
+            // console.log('target1=', target); //TRACE
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            console.log('target2=', target); //TRACE
+            // console.log('target2=', target); //TRACE
             // Does a scroll target exist?
             if (target.length) {
               // Only prevent default if animation is actually gonna happen
               event.preventDefault();
+              console.log('Link scroll down=', target.offset().top)
               $('html, body').animate({
-                scrollTop: target.offset().top  //{scrollTop: targetOffset - 100} ?
+                scrollTop: target.offset().top //{scrollTop: targetOffset - 100} ?
               }, 1000, function() {
                 // Callback after animation
                 // Must change focus!
                 var $target = $(target); //refers to the jQuery representation of the dom object
                 $target.focus();
-                console.log('$target=', $target); //TRACE
-                console.log('$target.is=', $target.is); //TRACE
+                // console.log('$target=', $target); //TRACE
+                // console.log('$target.is=', $target.is); //TRACE
                 if ($target.is(":focus")) { // Checking if the target was focused
                   return false;
                 } else {
